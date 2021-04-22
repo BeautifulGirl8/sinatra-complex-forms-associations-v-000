5158275b69e037fcc4bda5873b4580bd40e1330b
Vicky  Tee <vttak9@gmail.com>
2021-04-22 09:33:43 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,14 +6,21 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.create(params[:pet])
+    
+        if !params[:owner][:name].empty?
+          @pet.owner = Owner.create(params[:owner])
+          @pet.save
   end
+  redirect to "pets/#{@pet.id}"
+end
 
+   
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
